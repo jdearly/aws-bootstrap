3014ef61325fbf75e35d8735581272315718fe44
Josh Early <josh.early@protonmail.com>
2023-02-02 19:15:55 -0500

    reset setup files

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 STACK_NAME=awsbootstrap
-REGION=us-east-1
+REGION=us-east-1 
 CLI_PROFILE=Josh
+EC2_INSTANCE_TYPE=t2.micro 
 
-EC2_INSTANCE_TYPE=t2.micro
-
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile Josh \
-  --query "Account" --output text`
-
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
-
-# Generate a personal access token with repo and admin:repo_hook
-#    permissions from https://github.com/settings/tokens
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
-GH_BRANCH=main
+GH_BRANCH=master
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile Josh --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+echo $CODEPIPELINE_BUCKET
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
+echo "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
@@ -27,25 +24,25 @@ aws cloudformation deploy \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# Deploy the CloudFormation template
+echo "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-echo -e "\n\n ======== Deploying main.yml ========="
-aws cloudformation deploy \
-    --region $REGION \
-    --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
-    --template-file main.yml \
-    --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
-      GitHubOwner=$GH_OWNER \
-      GitHubRepo=$GH_REPO \
-      GitHubBranch=$GH_BRANCH \
-      GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-      CodePipelineBucket=$CODEPIPELINE_BUCKET
-
-
+    # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile Josh \
